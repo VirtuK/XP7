b49857d5ebd8351b2b561b313e37e5cc09c1a885
VirtuK <vitorkuiava.vc@gmail.com>
2025-03-16 00:03:46 -0300

    Highlight no personagem quando passa atras de objetos

---
+++ Assets/Scripts/PlayerOutline.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerOutline : MonoBehaviour
+{
+    public GameObject outlineObject; // The duplicate player model with the outline material
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        outlineObject.SetActive(false); // Hide outline by default
+    }
+
+    void Update()
+    {
+        CheckIfBehindWall();
+    }
+
+    void CheckIfBehindWall()
+    {
+        Vector3 cameraPosition = mainCamera.transform.position;
+        Vector3 direction = transform.position - cameraPosition;
+
+        RaycastHit hit;
+        if (Physics.Raycast(cameraPosition, direction, out hit))
+        {
+            
+            if (hit.collider.gameObject != gameObject)
+            {
+                outlineObject.SetActive(true);
+                // Show the outline if behind an object
+            }
+            else
+            {
+                outlineObject.SetActive(false); // Hide if the player is visible
+            }
+        }
+    }
+}
